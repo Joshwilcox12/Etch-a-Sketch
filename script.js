af70a326b0dda696efab0aaab4f105920c2f3e25
Josh Wilcox <joshuawilcox63@gmail.com>
2024-09-01 14:31:36 -0500

    Add grid to page
    Created a function that generates 16x16 grid, though right now it's all in 1 row

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const container = document.querySelector('#container');
+
+
+
+
+
+
+
+
+function createGrid(){
+    for(i = 0; i <= 256; i++){
+       
+        const squareDiv = document.createElement("div");
+        container.appendChild(squareDiv);
+        squareDiv.style.height = '35px';
+        squareDiv.style.width = '35px';
+        squareDiv.style.borderStyle = 'solid';
+        squareDiv.style.borderColor = 'red';
+        squareDiv.style.flex = '1';
+
+    }
+
+}
+
+createGrid();
 
