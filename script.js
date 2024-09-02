29e83ca65eddad88bf7bc60e1c4e9eceedf70e6c
Josh Wilcox <joshuawilcox63@gmail.com>
2024-09-02 16:03:42 -0500

    Create Grid 16x16
    Created a grid 16x16 and with hover to fill each div to black.
    also made it a bit with style.
    Next is to create button that will generate new grid.

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ const container = document.querySelector('#container');
 
 
 
-function createGrid(){
-    for(i = 0; i <= 256; i++){
+
+    for(i = 0; i < 256; i++){
+        const gridDiv = document.createElement("div");
+
+        gridDiv.style.cssText = 'height: 37.5px; width: 37.5px;'
+        container.appendChild(gridDiv);
+
+        gridDiv.addEventListener("mouseover",() => {
+            gridDiv.style.cssText = 'height: 37.5px; width: 37.5px; background-color: black;'
+        })
+        
+
+
+
+
+
        
-        const squareDiv = document.createElement("div");
-        container.appendChild(squareDiv);
-        squareDiv.style.height = '35px';
-        squareDiv.style.width = '35px';
-        squareDiv.style.borderStyle = 'solid';
-        squareDiv.style.borderColor = 'red';
-        squareDiv.style.flex = '1';
 
     }
 
-}
-
-createGrid();
 
