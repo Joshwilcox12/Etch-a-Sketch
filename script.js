9ea4add7cdfac9badb0c58c54f795b95c0667ad5
Josh Wilcox <joshuawilcox63@gmail.com>
2024-09-10 18:52:43 -0500

    add comments and limit grid values
    added restrictions to user input to between 16 and 100

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+// Select html elements
 const container = document.querySelector('#container');
 const body = document.querySelector('body');
 const resizeBtn = document.querySelector("#gridBtn");
 
 
-
+// add an event lister to button
 resizeBtn.addEventListener("click",newGrid);
 
 
 
 
 
-
+//create gird
     for(i = 0; i < 256; i++){
         const gridDiv = document.createElement("div");
         gridDiv.id = 'grid';
@@ -25,20 +26,31 @@ resizeBtn.addEventListener("click",newGrid);
 
 
    
-
+// create new grid from user
     function newGrid(){
         
         
        
-        
+        //remove element that was previously there (grid)
         container.innerHTML = '';
-
+        //get user choice and make sure they only enter between 16 and 100
+        let userChoice;
         let promptUser = prompt('Enter a value between 16 and 100 to resize the grid!');
-        console.log(promptUser);
-        let newGridDim = promptUser * promptUser;
-        console.log(newGridDim)
-        let newDivDim = 600 / promptUser;
-        console.log(newDivDim)
+
+        if (promptUser < 16){
+            alert('Please enter a correct value!')
+        }
+        else if(promptUser > 100){
+            alert('Please enter a correct value!')
+        }
+        else{
+            userChoice = promptUser;
+        }
+        
+        let newGridDim = userChoice * userChoice;
+        
+        let newDivDim = 600 / userChoice;
+        // convert to string so we can use it for css 
         String(newDivDim);
 
         for(i = 0; i < newGridDim; i++){
